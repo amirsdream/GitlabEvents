42e3cc62c8b398b7cbecd19718f832d3d2aa7a2e
amir <amirsdream@gmail.com>
2021-04-06 12:35:28 +0200

    "Initial Commit"

---
+++ flaskr/checkevents.py
@@ -0,0 +1,41 @@
+from flask import request, jsonify, Flask, abort
+import requests
+import json
+from pprint import pprint
+import os
+import time
+
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+# if not SECRET_KEY:
+#     raise ValueError("No SECRET_KEY set for Flask application")
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/db_test.yml/', methods=['GET'])
+def getevents():
+    time.sleep(120)
+    response= \
+"""
+db_test1:
+    tags: 
+        - runner
+    when: manual
+    script:
+        - echo 1
+        - echo 2
+"""
+    return response
+
+@app.route('/', methods=['POST'])
+def postevents():
+    try:
+        record = json.loads(request.data)
+        # pprint (record)
+        # r = requests.post("http://192.168.0.73/api/v4/projects/1/trigger/pipeline",data={'token':'188372ead06795298ace49d0448aaf','ref':'master','variables[test]':'true'})
+        # pprint(r,"output of requests")
+        return jsonify(record)
+    except:
+        abort(400)
+
+app.run(host="0.0.0.0")
